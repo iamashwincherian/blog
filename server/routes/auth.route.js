c581ea3a87560afc0516170b83c050a0f30bfdf0
Ashwin Cherian Joseph <iamashwincherian@gmail.com>
2022-05-09 21:31:32 +0530

    Seperate auth route

---
+++ server/routes/auth.route.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+
+const User = require("../models/user.model");
+
+const router = express.Router();
+
+// Authentication
+
+const signUserToken = ({ username, name }) => {
+    return jwt.sign({ username, name }, process.env.JWT_PRIVATE_KEY);
+};
+
+router.post("/login", async (req, res) => {
+    const { username, password } = req.body;
+
+    const user = await User.findOne({
+        $or: [{ username }, { email: username }],
+    });
+    if (!user) throw Error("User not found");
+
+    const correctPassword = await bcrypt.compare(password, user.hash);
+    if (!correctPassword) throw Error("Incorrect Password!");
+
+    const token = signUserToken(user);
+
+    res.send({
+        token,
+        user,
+    });
+});
+
+router.post("/register", async (req, res) => {
+    const { name, email, password } = req.body;
+
+    const username = email.split("@")[0];
+    const hash = await bcrypt.hash(password, process.env.BCRYPT_SALT_ROUNDS);
+    const user = await User.create({
+        name,
+        email,
+        username,
+        hash,
+    });
+    const token = signUserToken(user);
+
+    res.send({
+        token,
+        user,
+    });
+});
+
+module.exports = router;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,59 +1,11 @@
-const express = require("express");
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const router = require("express").Router();
+const authRoutes = require("./auth.route");
 
-const User = require("../models/user.model");
-
-const router = express.Router();
-const SALT_ROUNDS = 10;
+router.use("/auth", authRoutes);
 
 router.get("/", (req, res) => {
     res.send({
-        data: "Blog",
-    });
-});
-
-// Authentication
-
-const signUserToken = ({ username, name }) => {
-    return jwt.sign({ username, name }, process.env.JWT_PRIVATE_KEY);
-};
-
-router.post("/login", async (req, res) => {
-    const { username, password } = req.body;
-
-    const user = await User.findOne({
-        $or: [{ username }, { email: username }],
-    });
-    if (!user) throw Error("User not found");
-
-    const correctPassword = await bcrypt.compare(password, user.hash);
-    if (!correctPassword) throw Error("Incorrect Password!");
-
-    const token = signUserToken(user);
-
-    res.send({
-        token,
-        user,
-    });
-});
-
-router.post("/register", async (req, res) => {
-    const { name, email, password } = req.body;
-
-    const username = email.split("@")[0];
-    const hash = await bcrypt.hash(password, SALT_ROUNDS);
-    const user = await User.create({
-        name,
-        email,
-        username,
-        hash,
-    });
-    const token = signUserToken(user);
-
-    res.send({
-        token,
-        user,
+        data: "Welcome to my Blog!",
     });
 });
 
