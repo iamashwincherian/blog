f8df77dab2d2676b578efabc03704ce606e8b3a4
Ashwin Cherian Joseph <iamashwincherian@gmail.com>
2022-05-10 07:32:41 +0530

    Add error handler

--- server/index.js
+++ server/index.js
@@ -15,6 +15,11 @@ const startServer = () => {
     app.use(express.json());
     app.use("/api", routes);
 
+    app.use((err, req, res, next) => {
+        if (!err) next();
+        res.status(500).send({ error: err.message });
+    });
+
     app.listen(3000, () => console.log("Server has started on port:", PORT));
 };
 
--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,20 +1,33 @@
-const express = require("express");
-const bcrypt = require("bcrypt");
-
-const User = require("../models/user.model");
+const router = require("express").Router();
 const AuthService = require("../services/auth.service");
 
-const router = express.Router();
-
-router.post("/login", async (req, res) => {
+router.post("/login", async (req, res, next) => {
     const { username, password } = req.body;
-    const { token, user } = await AuthService.login(username, password);
+    let token, user;
+
+    try {
+        const response = await AuthService.login(username, password);
+        token = response.token;
+        user = response.user;
+    } catch (err) {
+        return next(err);
+    }
+
     res.send({ token, user });
 });
 
-router.post("/register", async (req, res) => {
+router.post("/register", async (req, res, next) => {
     const { name, email, password } = req.body;
-    const { token, user } = await AuthService.register(name, email, password);
+    let token, user;
+
+    try {
+        const response = await AuthService.register(name, email, password);
+        token = response.token;
+        user = response.user;
+    } catch (err) {
+        return next(err);
+    }
+
     res.send({ token, user });
 });
 
