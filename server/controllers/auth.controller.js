cc01ae1335402155b95c7b1f1b6a8a06d6cd3a38
Ashwin Cherian Joseph <iamashwincherian@gmail.com>
2022-05-19 21:28:45 +0530

    Format spacing

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -3,7 +3,7 @@ const AuthService = require("../services/auth.service");
 const login = async (req, res, next) => {
     const { username, password } = req.body;
     let token, user;
-    
+
     try {
         const response = await AuthService.login(username, password);
         token = response.token;
@@ -11,14 +11,14 @@ const login = async (req, res, next) => {
     } catch (err) {
         return next(err);
     }
-    
+
     res.send({ token, user });
-}
+};
 
 const register = async (req, res, next) => {
     const { name, email, password } = req.body;
     let token, user;
-    
+
     try {
         const response = await AuthService.register(name, email, password);
         token = response.token;
@@ -26,11 +26,8 @@ const register = async (req, res, next) => {
     } catch (err) {
         return next(err);
     }
-    
-    res.send({ token, user });
-}
 
-module.exports = {
-    login,
-    register
+    res.send({ token, user });
 };
+
+module.exports = { login, register };
